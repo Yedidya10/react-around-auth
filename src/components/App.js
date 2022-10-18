026955341371d368b5ddd5bc49cf29c3fc71b348
Yedidya <yedidya.abergel@gmail.com>
2022-10-18 18:50:49 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,318 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import ImagePopup from './ImagePopup';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import api from '../utils/api.js';
+import EditProfilePopup from './EditProfilePopup';
+import EditAvatarPopup from './EditAvatarPopup';
+import AddPlacePopup from './AddPlacePopup';
+import DeletePopup from './DeletePopup';
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import ProtectedRoute from './ProtectedRoute';
+import InfoTooltip from './InfoTooltip';
+import * as auth from '../utils/auth';
+
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
+
+  const [infoTooltipType, setInfoTooltipType] = useState('');
+
+  const [cards, setCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState({
+    name: '',
+    link: '',
+  });
+
+  const [currentUser, setCurrentUser] = useState({});
+  const [userData, setUserData] = useState({
+    email: 'email@mail.com',
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
+  const history = useHistory();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((user) => {
+        setCurrentUser(user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    api
+      .getInitialCards()
+      .then((res) => {
+        setCards(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      setIsLoading(true);
+      auth
+        .checkToken(jwt)
+        .then((res) => {
+          if (res.data._id) {
+            setLoggedIn(true);
+            setUserData({ email: res.data.email });
+            history.push('/react-around-auth');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          history.push('/signin');
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [history]);
+
+  const handleEditAvatarClick = () => {
+    setIsEditAvatarPopupOpen(true);
+  };
+
+  const handleEditProfileClick = () => {
+    setIsEditProfilePopupOpen(true);
+  };
+
+  const handleAddPlaceClick = () => {
+    setIsAddPlacePopupOpen(true);
+  };
+
+  const closeAllPopups = () => {
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsImagePopupOpen(false);
+    setIsDeletePopupOpen(false);
+    setIsInfoTooltipOpen(false);
+  };
+
+  const handleCardClick = (card) => {
+    setIsImagePopupOpen(true);
+    setSelectedCard({
+      name: card.name,
+      link: card.link,
+    });
+  };
+
+  const handleCardDeleteClick = (card) => {
+    setIsDeletePopupOpen(true);
+    setSelectedCard(card);
+  };
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((user) => user._id === currentUser._id);
+
+    api
+      .toggleLike(card._id, isLiked)
+      .then((newCard) => {
+        setCards((cards) =>
+          cards.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const handleCardDelete = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    api
+      .deleteCard(selectedCard._id)
+      .then(() => {
+        const newCards = cards.filter(
+          (currentCard) => currentCard._id !== selectedCard._id
+        );
+        setCards(newCards);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleUpdateUser = ({ name, about }) => {
+    setIsLoading(true);
+    api
+      .setUserInfo({ name, about })
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleUpdateAvatar = (url) => {
+    setIsLoading(true);
+    api
+      .setUserAvatar(url)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  function handleAddPlaceSubmit(card) {
+    setIsLoading(true);
+    api
+      .createCard(card)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }
+
+  function handleRegister({ email, password }) {
+    setIsLoading(true);
+    auth
+      .register(email, password)
+      .then((res) => {
+        if (res.data._id) {
+          setInfoTooltipType('successful');
+          history.push('/signin');
+        } else {
+          setInfoTooltipType('failed');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setInfoTooltipType('failed');
+        setIsInfoTooltipOpen(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleLogin({ email, password }) {
+    setIsLoading(true);
+    auth
+      .login(email, password)
+      .then((res) => {
+        if (res.token) {
+          setLoggedIn(true);
+          setUserData({ email });
+          localStorage.setItem('jwt', res.token);
+          history.push('/react-around-auth');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setInfoTooltipType('failed');
+        setIsInfoTooltipOpen(true);
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+  function handleSignout() {
+    setLoggedIn(false);
+    localStorage.removeItem('jwt');
+    history.push('/signin');
+  }
+
+  return (
+    <div className="App">
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header
+          loggedIn={loggedIn}
+          email={userData.email}
+          handleSignout={handleSignout}
+        />
+        <Switch>
+          <ProtectedRoute exact path="/react-around-auth" loggedIn={loggedIn}>
+            <Main
+              onEditProfileClick={handleEditProfileClick}
+              onAddPlaceClick={handleAddPlaceClick}
+              onEditAvatarClick={handleEditAvatarClick}
+              cards={cards}
+              onCardClick={handleCardClick}
+              onCardLike={handleCardLike}
+              onCardDeleteClick={handleCardDeleteClick}
+            />
+          </ProtectedRoute>
+
+          <Route path="/signup">
+            <Register handleRegister={handleRegister} isLoading={isLoading} />
+          </Route>
+
+          <Route path="/signin">
+            <Login handleLogin={handleLogin} isLoading={isLoading} />
+          </Route>
+
+          <Route>
+            {loggedIn ? (
+              <Redirect to="/react-around-auth" />
+            ) : (
+              <Redirect to="/signin" />
+            )}
+          </Route>
+        </Switch>
+
+        <Footer />
+
+        <EditProfilePopup
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser}
+          isLoading={isLoading}
+        />
+
+        <EditAvatarPopup
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onUpdateAvatar={handleUpdateAvatar}
+          isLoading={isLoading}
+        />
+
+        <AddPlacePopup
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onAddPlaceSubmit={handleAddPlaceSubmit}
+          isLoading={isLoading}
+        />
+
+        <DeletePopup
+          isOpen={isDeletePopupOpen}
+          onClose={closeAllPopups}
+          onSubmitDelete={handleCardDelete}
+          isLoading={isLoading}
+        />
+
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopupOpen}
+          onClose={closeAllPopups}
+          name="image"
+        />
+
+        <InfoTooltip
+          isOpen={isInfoTooltipOpen}
+          onClose={closeAllPopups}
+          type={infoTooltipType}
+          name="tooltip"
+        />
+      </CurrentUserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+const Card = (props) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwner = props.card.owner._id === currentUser._id;
+
+  const isLiked = props.card.likes.some((user) => user._id === currentUser._id);
+
+  const cardLikeButtonClassName = `card__like-button ${
+    isLiked && "card__like-button_active"
+  }`;
+
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
+
+  function handleLikeClick() {
+    props.onCardLike(props.card);
+  }
+
+  function handleDeleteCard() {
+    props.onCardDelete(props.card);
+  }
+
+  return (
+    <li className="card">
+      {isOwner && (
+        <button
+          className="card__delete-button"
+          type="button"
+          onClick={handleDeleteCard}
+        />
+      )}
+      <img
+        className="card__image"
+        src={props.card.link}
+        alt={props.card.name}
+        onClick={handleClick}
+      />
+      <div className="card__title-wrapper">
+        <h2 className="card__title">{props.card.name}</h2>
+        <div className="card__likes-wrapper">
+          <button
+            className={cardLikeButtonClassName}
+            type="button"
+            onClick={handleLikeClick}
+          />
+          <span className="card__likes-count">{props.card.likes.length}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default Card;
